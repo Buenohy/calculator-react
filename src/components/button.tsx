9bac0c590f250ea48f2a5e87fc747b9cb308adf3
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-26 14:51:42 -0300

    feat: Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,27 +3,31 @@ import { tv, type VariantProps } from 'tailwind-variants';
 import Text from './text';
 
 const buttonVariants = tv({
-  base: 'flex items-center justify-center rounded-xl p-3 cursor-pointer text-(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover) shadow-(--shadow)',
-
+  base: 'flex items-center justify-center rounded-xl p-3 cursor-pointer text-(--text) bg-linear-(--gradient) hover:bg-linear-(--gradient-hover) shadow-(--shadow) border-none',
   variants: {
     variant: {
       default: 'bg-(--background)',
       primary: 'bg-(--primary)',
     },
+    size: {
+      sm: 'p-2 text-sm',
+      md: 'p-3 text-base',
+    },
   },
-
   defaultVariants: {
     variant: 'default',
+    size: 'md',
   },
 });
 
 interface ButtonProps
   extends
-    Omit<React.ComponentProps<'button'>, 'size' | 'disabled'>,
+    Omit<React.ComponentProps<typeof Text>, 'variant' | 'size'>,
     VariantProps<typeof buttonVariants> {}
 
 export default function Button({
   variant,
+  size,
   className,
   children,
   ...props
@@ -32,7 +36,7 @@ export default function Button({
     <Text
       as="button"
       variant="heading"
-      className={buttonVariants({ variant, className })}
+      className={buttonVariants({ variant, size, className })}
       {...props}
     >
       {children}
