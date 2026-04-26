96636f4b0914f07c1ecb38d71ca13c222c9f78eb
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-26 13:43:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="py-28 px-4 sm:px-10 flex flex-col sm:flex-row items-center sm:items-stretch gap-2">
+      <h1>Caculator React</h1>
+    </main>
+  );
+}
