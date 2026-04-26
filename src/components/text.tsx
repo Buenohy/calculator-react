f60875ba16f0e0c9db56077353c5e26b3fbe9eaf
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-26 14:03:04 -0300

    feat: install tailwind-merge and created Text component

---
+++ src/components/text.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+
+const textVariants = tv({
+  base: 'text-white',
+  variants: {
+    variant: {
+      default: 'text-xl',
+      muted: 'text-xl text-(--text-secondary)',
+      heding: 'text-2xl',
+      blast: 'text-3xl',
+    },
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Text({
+  as = 'span',
+  variant = 'default',
+  className,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: textVariants({ variant, className }),
+      ...props,
+    },
+    children,
+  );
+}
